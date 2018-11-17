8854711a5366e1dc590d77e229370ef51c094594
shiqiangqiang <1261754629@qq.com>
2018-11-17 23:12:38 +0800

    rocketmq-example第一次提交git

---
+++ src/main/java/com/alibaba/rocketmq/example/simple/CachedQueue.java
@@ -0,0 +1,15 @@
+package main.java.com.alibaba.rocketmq.example.simple;
+
+import java.util.TreeMap;
+
+import com.alibaba.rocketmq.common.message.MessageExt;
+
+
+public class CachedQueue {
+    private final TreeMap<Long, MessageExt> msgCachedTable = new TreeMap<Long, MessageExt>();
+
+
+    public TreeMap<Long, MessageExt> getMsgCachedTable() {
+        return msgCachedTable;
+    }
+}
